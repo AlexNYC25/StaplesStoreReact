c03ff0b14953c1153e3012f8849fc3aecc2906e9
Alexis Montes <montes.alexis25@gmail.com>
2020-06-25 20:42:49 -0400

    testing change

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <p>Test statment</p>
   </React.StrictMode>,
   document.getElementById('root')
 );
