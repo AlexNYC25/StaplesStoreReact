cc8347acaedf64422c4bde88d324d4c95ef29b99
Alexis Montes <montes.alexis25@gmail.com>
2020-06-25 22:08:39 -0400

    Revert "created basic navbar"
    This reverts commit 46f6823ca0f97ab28360076105fcc5ad8789bc59.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import NavBar from './components/Navbar/NavBar'
+//import NavBar from './components/Navbar/NavBar'
 
 ReactDOM.render(
   <React.StrictMode>
-    <NavBar />
+    
     <App />
     <p>Test statment</p>
   </React.StrictMode>,
