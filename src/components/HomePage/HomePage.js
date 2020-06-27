00d0aa57d4edf90ce32b18bbae3ebf9f476ba718
Alexis Montes <montes.alexis25@gmail.com>
2020-06-26 22:16:35 -0400

    set up basic homepage layout using productlayout component

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+//import '../App.css'
+
+import ProductLayout from './ProductLayout/ProductLayout'
+
+class HomePage extends React.Component {
+
+    render(){
+        return (
+            <div className="App">
+                
+                <ProductLayout/>
+            </div>
+        );
+    }
+}
+
+export default HomePage
