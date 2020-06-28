7b3deba9000f64a8f34a1cbfb87abe1e348320b2
Alexis Montes <montes.alexis25@gmail.com>
2020-06-28 13:05:19 -0400

    added browser router compoment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Router } from 'react-router-dom'
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,9 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/">
-            <HomePage />
-          </Route>
+          <Route path="/" component={HomePage} />
 
         </Switch>
       </Router>
