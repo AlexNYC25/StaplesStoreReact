d429297c8a304cbf2d4a15770c730086af38228f
Alexis Montes <montes.alexis25@gmail.com>
2020-06-28 00:08:16 -0400

    created basic footer component to start with

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+function Footer() {
+    return(
+        <footer class="page-footer font-small blue pt-4">
+            <div class="container-fluid text-center text-md-left">
+                <div class="row">
+                    <p>test text</p>
+                </div>
+            </div>
+            
+        </footer>
+
+    )
+}
+
+export default Footer;
