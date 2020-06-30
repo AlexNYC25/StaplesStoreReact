f66e820a93d94ca4b12677654df3164910b712e7
Alexis Montes <montes.alexis25@gmail.com>
2020-06-29 21:25:11 -0400

    added Navbar and Footer components in Router and DataManipulation Route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
-import logo from './logo.svg';
+
 import './App.css';
 
+import NavBar from './components/NavBar/NavBar'
+import Footer from './components/Footer/Footer'
+
 import HomePage from './components/HomePage/HomePage'
+import DataManipulation from './components/DataMainpulation/DataManipulation'
+
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" component={HomePage} />
+	<div className="App">
+	
+	  <Router>
+	  	<NavBar />
+		<Switch>
+			<Route exact path="/" component={HomePage} />
+			<Route path="/DataManipulation" component={DataManipulation} />
 
-        </Switch>
-      </Router>
-        
-    </div>
+		</Switch>
+		<Footer />
+	  </Router>
+	  
+		
+	</div>
   );
 }
 
