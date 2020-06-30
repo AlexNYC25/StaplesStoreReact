1ed59a692a803296f427176b1d0f66620606752f
Alexis Montes <montes.alexis25@gmail.com>
2020-06-29 21:33:26 -0400

    Added Link component to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Link} from 'react-router-dom'
 
 
 class Navbar extends React.Component{
@@ -9,14 +10,19 @@ class Navbar extends React.Component{
                 <div>
             
                 </div>
-                <nav className="my-2 my-md-0 mr-md-3 navbar-text ">
-                    
-                    <a className="p-2 " href="/">All Items</a>
-                    <a className="p-2" href="/">Displays</a>
-                    <a className="p-2" href="/">Github</a>
-                    <a className="p-2" href="/DataManipulation">Data Control Panel</a>
-                    
-                </nav>
+                
+                    <nav className="my-2 my-md-0 mr-md-3 navbar-text ">
+                        
+                        <Link to="/">
+                            All Items
+                        </Link>
+                        <a className="p-2" href="/">Displays</a>
+                        <a className="p-2" href="/">Github</a>
+                        <Link to="/DataManipulation">
+                            Data Control Panel
+                        </Link>
+                    </nav>
+                
             </div>
         );
     }ß
