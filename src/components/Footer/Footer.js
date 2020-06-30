53ae6e75bb4deae0f45fe493ea4e51bd3235e767
Alexis Montes <montes.alexis25@gmail.com>
2020-06-29 21:31:43 -0400

    Filled in initial data for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 function Footer() {
     return(
-        <footer class="page-footer font-small blue pt-4">
+        <footer class="page-footer font-small blue pt-4 footer">
             <div class="container-fluid text-center text-md-left">
                 <div class="row">
                     <div class="col-md-6 mt-md-0 mt-3">
-                        <h5 class="text-uppercase">Footer Content</h5>
-                        <p>Here you can use rows and columns to organize your footer content.</p>
+                        <h5 class="text-uppercase">About</h5>
+                        <p>A little info about the page</p>
                     </div>
                     <hr class="clearfix w-100 d-md-none pb-3"></hr>
                     <div class="col-md-3 mb-md-0 mb-3">
-                        <h5 class="text-uppercase">Links</h5>
+                        <h5 class="text-uppercase">Tech used</h5>
                         <ul class="list-unstyled">
                             <li>
-                                <a href="#!">Link 1</a>
+                                <a href="#!">React JS</a>
                             </li>
 
                         </ul>
@@ -23,7 +23,7 @@ function Footer() {
                         <h5 class="text-uppercase">Links</h5>
                         <ul class="list-unstyled">
                             <li>
-                                <a href="#!">Link 1</a>
+                                <a href="#!">Bootstrap</a>
                             </li>
 
                         </ul>
@@ -31,7 +31,7 @@ function Footer() {
                 </div>
             </div>
 
-            <div class="footer-copyright text-center py-3">© 2020 Copyright:
+            <div class="footer-copyright text-center py-3">Coded with Love and coffee
                 <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
             </div>
             
